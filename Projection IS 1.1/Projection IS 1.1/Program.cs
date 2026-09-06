0f778b53cc13ec6fa2fff2292b5e785506195bcc
iliyavarygin <iliyavarygin@gmail.com>
2026-09-06 13:09:58 +0700

    Добавил проект Projection IS 1.1

---
+++ Projection IS 1.1/Projection IS 1.1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Projection_IS_1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
